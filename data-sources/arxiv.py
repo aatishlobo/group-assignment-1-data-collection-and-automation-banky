7994e82b89c3fa325b7068c93b19013f78c358b0
“jade-tseng” <jadeyutseng@gmail.com>
2025-09-30 16:46:44 -0700

    trial push, just rename filename

--- data-sources/arxiv.py
+++
@@ -1,3 +0,0 @@
-## calls arxiv API to fetch data on ML papers 
-# note to team: arxiv is where the industry reads state-of-the-art ML papers. we can use this data to find different categories of AI/ML research
-# (like voice, computer vision, NLP etc.) and compare this to the economic index data on what kind of tasks people use LLMs for.
